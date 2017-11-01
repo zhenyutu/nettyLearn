158ed7487375b9c953f8d819687f779838c7d13c
zhenyutu <1572629866@qq.com>
2017-11-01 15:26:52 +0800

    完成客户端和服务端的构建

---
+++ src/main/java/cn/tzy/chapter12/MarshallingCodecFactory.java
@@ -0,0 +1,30 @@
+
+package cn.tzy.chapter12;
+
+import org.jboss.marshalling.*;
+
+import java.io.IOException;
+
+public class MarshallingCodecFactory {
+
+    protected static Marshaller buildMarshalling() throws IOException {
+	final MarshallerFactory marshallerFactory = Marshalling
+		.getProvidedMarshallerFactory("serial");
+	final MarshallingConfiguration configuration = new MarshallingConfiguration();
+	configuration.setVersion(5);
+	Marshaller marshaller = marshallerFactory
+		.createMarshaller(configuration);
+	return marshaller;
+    }
+
+
+    protected static Unmarshaller buildUnMarshalling() throws IOException {
+	final MarshallerFactory marshallerFactory = Marshalling
+		.getProvidedMarshallerFactory("serial");
+	final MarshallingConfiguration configuration = new MarshallingConfiguration();
+	configuration.setVersion(5);
+	final Unmarshaller unmarshaller = marshallerFactory
+		.createUnmarshaller(configuration);
+	return unmarshaller;
+    }
+}
---
+++ src/main/java/cn/tzy/chapter12/NettyMessage.java
@@ -0,0 +1,26 @@
+package cn.tzy.chapter12;
+
+/**
+ * Created by tuzhenyu on 17-11-1.
+ * @author tuzhenyu
+ */
+public class NettyMessage {
+    private Header header;
+    private Object body;
+
+    public Header getHeader() {
+        return header;
+    }
+
+    public void setHeader(Header header) {
+        this.header = header;
+    }
+
+    public Object getBody() {
+        return body;
+    }
+
+    public void setBody(Object body) {
+        this.body = body;
+    }
+}

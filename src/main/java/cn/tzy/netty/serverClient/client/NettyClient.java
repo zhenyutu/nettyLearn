ef8638cbb8077af4e76ec8918fe694167636e865
zhenyutu <1572629866@qq.com>
2018-04-21 16:31:26 +0800

    完成客户端和服务端基础框架的测试

--- src/main/java/cn/tzy/netty/serverClient/client/NettyClient.java
+++ src/main/java/cn/tzy/netty/serverClient/client/NettyClient.java
@@ -1,10 +1,9 @@
 package cn.tzy.netty.serverClient.client;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelPipeline;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
 
 import java.net.SocketAddress;
 
@@ -19,7 +18,7 @@ public class NettyClient extends NettyTcpConnector{
         bootstrap.handler(new ChannelInitializer<Channel>() {
             protected void initChannel(Channel ch) throws Exception {
                 final ChannelPipeline pipeline = ch.pipeline();
-
+                pipeline.addLast(new TimeClientHandler());
             }
         });
 
@@ -31,5 +30,40 @@ public class NettyClient extends NettyTcpConnector{
         }
 
         return future;
+    }
+
+    public static void main(String[] args) {
+        NettyClient client = new NettyClient();
+        ChannelFuture future = client.connect("127.0.0.1",8008);
+        try {
+            future.channel().closeFuture().sync();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
+
+class TimeClientHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        byte[] bytes = "QUERY TIME ORDER".getBytes();
+        ByteBuf buf = Unpooled.buffer(bytes.length);
+        buf.writeBytes(bytes);
+        ctx.writeAndFlush(buf);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf)msg;
+        byte[] bytes = new byte[buf.readableBytes()];
+        buf.readBytes(bytes);
+        String body = new String(bytes,"UTF-8");
+        System.out.println("the server says: "+ body);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
     }
 }
--- src/main/java/cn/tzy/netty/serverClient/server/NettyServer.java
+++ src/main/java/cn/tzy/netty/serverClient/server/NettyServer.java
@@ -1,12 +1,12 @@
 package cn.tzy.netty.serverClient.server;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelPipeline;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
 
 import java.net.SocketAddress;
+import java.util.Date;
 
 /**
  * Created by tuzhenyu on 18-4-19.
@@ -30,7 +30,7 @@ public class NettyServer extends NettyTcpAcceptor{
             @Override
             protected void initChannel(Channel ch) throws Exception {
                 ChannelPipeline pipeline =ch.pipeline();
-
+                pipeline.addLast(new TimeServerHandler());
             }
         });
 
@@ -38,7 +38,7 @@ public class NettyServer extends NettyTcpAcceptor{
     }
 
     public static void main(String[] args) {
-        NettyServer server = new NettyServer(10000);
+        NettyServer server = new NettyServer(8008);
 
         try {
             server.start();
@@ -47,4 +47,25 @@ public class NettyServer extends NettyTcpAcceptor{
         }
 
     }
+}
+
+class TimeServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf)msg;
+        byte[] bytes = new byte[buf.readableBytes()];
+        buf.readBytes(bytes);
+        String body = new String(bytes,"UTF-8");
+        System.out.println("the client says: "+ body);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body)?new Date().toString():"BAD ORDER";
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.writeAndFlush(resp);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+
 }
